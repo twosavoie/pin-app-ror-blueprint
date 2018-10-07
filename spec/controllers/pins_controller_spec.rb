7941fbd6c541a94157a32a8d2f90c7392cb20fe9
lsavoie <twosavoie@gmail.com>
2018-10-06 21:48:14 -0500

    tests

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
 RSpec.describe PinsController do
 
+  #here I will describe what I expect the GET index controller action to do
+  describe "GET index" do
+
+    #in plain English, what I expect to happen
+    it 'renders the index template' do
+
+      #ask RSpec to run whatever action you need done
+      get :index
+
+      #my expectation of what will happen when RSpec runs the action
+      #this is the core of the RSpec testing framework
+      #can expect .to matcher or .not_to matcher
+      # keyword expect > what actually happened vs what you expected what happened to either match or not match
+      expect(response).to render_template("index")
+    end
+  end
+
 end
