567cb0cb2cfb53399043be6c9dc360f9febaddae
lsavoie <twosavoie@gmail.com>
2018-10-09 19:12:21 -0500

    Finish adding categories

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  validates_presence_of :title, :url, :slug, :text, :category_id
+  validates_presence_of :title, :url, :slug, :text, :name
   validates_uniqueness_of :slug
   belongs_to :category
 end
