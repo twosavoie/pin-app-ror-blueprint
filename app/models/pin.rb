87f71ba8eadf0c8939df9dd0b4448522ad0f5a78
lsavoie <twosavoie@gmail.com>
2018-10-17 00:47:45 -0500

    Paperclip and image upload ability

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
 class Pin < ActiveRecord::Base
-  validates_presence_of :title, :url, :slug, :text, :resource_type # :category_id # resource_type makes test pass but app doens't work
+  validates_presence_of :title, :url, :slug, :text, :category_id # :resource_type # resource_type makes test pass but app doens't work
   validates_uniqueness_of :slug
   belongs_to :category #, inverse_of: :pins # inverse needed? don't seem to
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: "http://placebear.com/300/300"
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
 end
