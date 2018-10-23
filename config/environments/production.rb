e0a9b1d413ff95b20d94b647e46a3669fae7c22c
lsavoie <twosavoie@gmail.com>
2018-10-22 23:05:01 -0500

    type fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
       bucket: ENV.fetch('AWS_BUCKET'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      s3_region: ENV.fetch('AWS_REGION')
     }
   }
   # Code is not reloaded between requests.
