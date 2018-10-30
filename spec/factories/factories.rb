28eb055e37612326e3493e8aa69f97c3b815e94f
lsavoie <twosavoie@gmail.com>
2018-10-30 12:36:10 -0500

    removed factories folder

--- spec/factories/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:email) { |n| "coder#{n}@skillcrush.com" }
-    first_name "Skillcrush"
-    last_name "Coder"
-    password "secret"
-  end
-end
